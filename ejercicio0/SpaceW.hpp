40b6733f619db47c303ecaaff32b22b66801e6e7
Eric Brandwein <brandweineric@gmail.com>
2019-03-25 16:34:55 -0300

    Proceso completo de excavación
    Antes se hacía solamente el girar una vez.

--- ejercicio0/SpaceW.hpp
+++ ejercicio0/SpaceW.hpp
@@ -1,27 +1,67 @@
+/*
+Cosas a mejorar:
+  - Agregar comentarios pertinentes.
+  - Fijarse que los Rpm y los Minutos sean positivos.
+  - Hacer interfaces en vez de hacer que cada clase dependa de las
+    implementaciones, y pasar las implementaciones como parámetros de
+    constructores
+  - Separar en archivos.
+*/
+
 enum class TipoPiso { Piedra, Polvo };
 enum class Porosidad {Poroso, Compacto};
 enum class Dureza {Duro, Blando};
+enum class Rotacion {Horaria, Antihoraria};
 
+using Rpm = unsigned;
+using Minutos = unsigned;
+
+class ConversorDeRotacion {
+public:
+  Rotacion RotacionContraria(Rotacion rotacion) const {
+    if (rotacion == Rotacion::Horaria) {
+      return Rotacion::Antihoraria;
+    } else {
+      return Rotacion::Horaria;
+    }
+  }
+};
+
+class Mecha {
+public:
+  void Girar(Rotacion rotacion, Rpm vueltas_por_minuto, Minutos minutos) const;
+};
+
+class Pinzas {
+public:
+  void Cerrar() const;
+  void Abrir() const;
+};
 
 class Brazo {
+  Mecha mecha;
+  Pinzas pinzas;
+  ConversorDeRotacion conversorDeRotacion;
 public:
-  using Rpm = unsigned;
-  using Minutos = unsigned;
-  enum class Rotacion { Horaria, Antihoraria };
-  void Girar(Rotacion, Rpm, Minutos) const;
+  void Excavar(Rotacion rotacion, Rpm vueltas_por_minuto, Minutos minutos) const {
+    mecha.Girar(rotacion, vueltas_por_minuto, minutos);
+    pinzas.Cerrar();
+    mecha.Girar(conversorDeRotacion.RotacionContraria(rotacion), vueltas_por_minuto, minutos);
+    pinzas.Abrir();
+  }
 };
 
 class Sensor {
 public:
-  Porosidad porosidadSueloActual() const;
-  Dureza durezaSueloActual() const;
+  Porosidad PorosidadSueloActual() const;
+  Dureza DurezaSueloActual() const;
 };
 
 class DetectorDePiso {
 public:
-  TipoPiso detectarTipoPiso(Sensor sensor) {
-    Porosidad porosidad = sensor.porosidadSueloActual();
-    Dureza dureza = sensor.durezaSueloActual();
+  TipoPiso DetectarTipoPiso(Sensor sensor) const {
+    Porosidad porosidad = sensor.PorosidadSueloActual();
+    Dureza dureza = sensor.DurezaSueloActual();
     if (porosidad == Porosidad::Poroso && dureza == Dureza::Blando) {
       return TipoPiso::Polvo;
     } else {
@@ -38,15 +78,11 @@ private:
 public:
 
   void Excavar() const {
-    TipoPiso tipoPiso = detectorDePiso.detectarTipoPiso(sensor);
+    TipoPiso tipoPiso = detectorDePiso.DetectarTipoPiso(sensor);
     if (tipoPiso == TipoPiso::Piedra) {
-
-      brazo.Girar(Brazo::Rotacion::Horaria, 150, 10);
-
+      brazo.Excavar(Rotacion::Horaria, 150, 10);
     } else if (tipoPiso == TipoPiso::Polvo) {
-
-      brazo.Girar(Brazo::Rotacion::Antihoraria, 100, 5);
-
+      brazo.Excavar(Rotacion::Antihoraria, 100, 5);
     }
   }
 
